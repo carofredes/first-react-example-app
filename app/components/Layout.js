7b075765320e5f9310439880cd862ed88fd9d0dd
Carolina Fredes <Carolina Fredes>
2017-01-26 18:03:48 -0300

    New app structure, layout, component

---
+++ app/components/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Layout extends React.Component {
+	render() {
+		return (
+			<div id='Layout' className='pure-g'>
+				<div className='pure-u-1-3'>First</div>
+				<div className='pure-u-1-3'>Second</div>
+				<div className='pure-u-1-3'>Third</div>
+			</div>	
+		);
+	}
+}
+
+export default Layout
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { 
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'stage-0', 'react' ],
-                    plugins: ['transform-runtime']
-                } 
-            }
-        ]
-    }
+  entry: {
+    app: './app',
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].js',
+    chunkFileName: '[id].js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0', 'react'],
+          plugins: ['transform-runtime', 'transform-decorators-legacy'],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css', {
+      allChunks: true,
+    }),
+  ],
 };
